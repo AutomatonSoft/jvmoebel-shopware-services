db133db6194e5675ed2a86004a163f04929d9f2b
Sergey Kovalev <kovalev.connect@gmail.com>
2026-08-12 21:09:19 +0300

    add commands to README.md

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # jvmoebel-services
+
+cp .env.example .env
+docker-compose up --build
+docker exec jvmoebel_backend uv run alembic upgrade head
--- app/main.py
+++ app/main.py
@@ -1,25 +1,24 @@
 from contextlib import asynccontextmanager
 
+import uvicorn
 from fastapi import FastAPI
 
-import uvicorn
-
 from core.logger import configure_logging
-
-
-
-#import core.db.models # разобраться зачем этот импорт. без него пятисотил поинт просмотра пользователя по айди
 from domains.ar.router import router as products_router
 
+
 configure_logging()
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
 
+
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(products_router)
+
 
 @app.get("/hello/")
 def hello(name: str = "World"):

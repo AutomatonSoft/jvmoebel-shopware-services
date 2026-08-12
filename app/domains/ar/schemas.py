19ec38094b87339f10370ae29d3f172c73ec16d9
Sergey Kovalev <kovalev.connect@gmail.com>
2026-08-12 12:56:12 +0300

    add ar schemas

---
+++ app/domains/ar/schemas.py
@@ -0,0 +1,30 @@
+# app/domains/ar_models/schemas.py
+
+from pydantic import BaseModel, ConfigDict
+
+
+class SARModelBase(BaseModel):
+    width: float
+    height: float
+    depth: float
+    unit: str
+
+
+class SARModelCreate(SARModelBase):
+    pass
+
+
+class SARModelUpdate(SARModelBase):
+    pass
+
+
+class SARModelRead(SARModelBase):
+    model_config = ConfigDict(
+        from_attributes=True,
+    )
+
+    id: int # по тз не нужно возвращать. уточнить
+    sku: str
+    status: str
+    file_format: str
+    file_path: str

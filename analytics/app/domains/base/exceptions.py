94f24e8a50b76a03542d9c3d34cbea71e8409b89
Sergey Kovalev <kovalev.connect@gmail.com>
2026-08-30 15:09:43 +0300

    copy core infrastructure from ar

---
+++ analytics/app/domains/base/exceptions.py
@@ -0,0 +1,56 @@
+from fastapi import HTTPException, status
+
+
+class BadRequestException(HTTPException):
+    def __init__(
+        self,
+        detail: str = "Bad request",
+    ) -> None:
+        super().__init__(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail=detail,
+        )
+
+
+class UnauthorizedException(HTTPException):
+    def __init__(
+        self,
+        detail: str = "Unauthorized",
+    ) -> None:
+        super().__init__(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=detail,
+        )
+
+
+class NotFoundException(HTTPException):
+    def __init__(
+        self,
+        detail: str = "Not found",
+    ) -> None:
+        super().__init__(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=detail,
+        )
+
+
+class AlreadyExistsException(HTTPException):
+    def __init__(
+        self,
+        detail: str = "Already exists",
+    ) -> None:
+        super().__init__(
+            status_code=status.HTTP_409_CONFLICT,
+            detail=detail,
+        )
+
+
+class InternalServerException(HTTPException):
+    def __init__(
+        self,
+        detail: str = "Internal server error",
+    ) -> None:
+        super().__init__(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=detail,
+        )
